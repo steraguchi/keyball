c3795621e40276472a32ff8b44955e2b4196827d
Shuichi Teraguti <teraguti@gmail.com>
2025-12-16 16:41:29 +0900

    Change to increase the number of layers from 4 to 5

--- qmk_firmware/keyboards/keyball/keyball44/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/config.h
@@ -74,6 +74,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define LAYER_STATE_8BIT
 #endif
 
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
 // To squeeze firmware size
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
